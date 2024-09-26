ba1612013c7969fddcb2e93ef5d55c9aac1cc1e2
Dev Cyth <hydra@Devs-MacBook-Pro.local>
2024-09-26 11:33:41 +0200

    weather app

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,16 +1,55 @@
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
 const Weather = () => {
 
+    const [data, setData] = useState(null)
+    const [search, setSearch] = useState('')
+    const [error, setError] = useState(null)
+
+
+
     const API_KEY = import.meta.env.VITE_API_KEY
-    console.log(API_KEY)
-    const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London`
+    const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${search}`
+
+
+    const fetchWeather = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await axios.get(URL)
+            setData(response.data)
+        }
+        catch (err) {
+            console.log(err)
+            setError(err)
+        }
+    }
+
+    // useEffect(() => {
+    //     fetchWeather()
+    // }, [])
+
+    console.log(data)
+
+    if(error) return <h1>{error.response.data.error.message}</h1>
 
     return (
         <>
-            <h1>Hello climat</h1>
+            <form onSubmit={fetchWeather} action="">
+                <input type="text" className='border-4' onChange={e => setSearch(e.target.value)} />
+                <input type="submit" />
+            </form>
+            {
+                data && (
+                    <>
+                        <h1>Hello from :  {data.location.name} </h1>
+                        <p>The current weather condition is : {data.current.condition.text} with : {data.current.temp_c} degrees</p>
+                    </>
+                )
+            }
+
         </>
     )
 
